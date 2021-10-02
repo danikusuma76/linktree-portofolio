40aae74a5abd8a647207a2a9d49ae91705e0c791
danikusuma76 <dani_kusumaaji@yahoo.com>
2021-10-02 20:12:00 +0700

    update

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 const links =[
-    {
-        label :"Whatsapp",
-        link :"wa.me/6289523677484",
     
-    },
     {
         label :"Instagram",
         link :"https://instagram.com/dani_kusuma42",
     
     },
-]
+    {
+        label :"Whatsapp",
+        link :"https://wa.me/6289523677484",
+    
+    },
+    {
+        label :"Github",
+        link :"https://github.com/danikusuma76",
+    
+    },
+    {
+        label :"Facebook",
+        link :"https://www.facebook.com/dani.deardlord/",
+    
+    },
+    {
+        label :"Linkedin",
+        link :"https://linkedin.com/in/dani-kusuma-aji-a52935208",
+    
+    },
+];
 const btnContainer = document.getElementById("btn-container");
 
-for (let i = 0; i < link.length; i++) {
+for (let i = 0; i < links.length; i++) {
     const btn = document.createElement("button");
     btn.classList.add("btn-link");
     btn.innerHTML = links[i].label;
-    btn.addEventListener("click",()=> clickLink(links[i].link))
-    btnContainer.append(btn)
-    
+    btn.addEventListener("click", () => clickLink(links[i].link));
+    btnContainer.append(btn);
 }
-function clickLink  (url)
-{
-    window.open(url,"blank");
+
+function clickLink(url) {
+    window.open(url, "_blank");
 }
